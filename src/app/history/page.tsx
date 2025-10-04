40581f57e9afb2c8c7b4286ab651d5d4901edf80
Chukwukadibia <chukwukadiemma@gmail.com>
2025-10-04 15:11:54 +0100

    update

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,11 +1,7 @@
-import { useEffect } from "react";
-
-const HowToPage: React.FC = () => {
-    useEffect(() => {
-        console.log("bing bom bam")
-        //if reference search params exist, verify transaction
-    }, [])
+const HistoryPage: React.FC = () => {
     return (
         <div>ORDER HISTORY PAGE</div>
     );
 }
+
+export default HistoryPage
